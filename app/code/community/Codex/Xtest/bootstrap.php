1c551181c109322fc84b65343833bbc5b1106074
Rafael Ortega Bueno <rafael@uecommerce.com.br>
2015-07-31 03:55:36 -0300

    test

--- app/code/community/Codex/Xtest/bootstrap.php
+++ app/code/community/Codex/Xtest/bootstrap.php
@@ -1,17 +1,15 @@
 <?php
-
 $mageFile = dirname( getcwd() ) . DIRECTORY_SEPARATOR . 'app' . DIRECTORY_SEPARATOR . 'Mage.php';
 if( file_exists( $mageFile ) ) {
 
     $_SERVER['SCRIPT_FILENAME'] = NULL;
     require_once $mageFile;
-    require_once __DIR__.DS.'Xtest.php';
+   // require_once __DIR__.DS.'Xtest.php';
 
-    Mage::app('admin', 'store', array( 'config_model' => 'Codex_Xtest_Model_Core_Config' ));
+    Mage::app('admin', 'store', array( 'config_model' => 'Mage_Core_Model_Config' ));
 
-    define('XTEST_BOOTSTRAPPED', true);
+    //define('XTEST_BOOTSTRAPPED', true);
 
 } else {
     throw new Exception("Mage.php not found.");
 }
-
